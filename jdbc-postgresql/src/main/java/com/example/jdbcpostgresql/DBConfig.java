2894c5b0bfccb3f0c6db6c36fd1455b34efe00e8
bakhtyar <muhammad.bakhtyar@intigral.net>
2025-05-20 00:10:35 +0500

    remove unused imports

--- jdbc-postgresql/src/main/java/com/example/jdbcpostgresql/DBConfig.java
+++ jdbc-postgresql/src/main/java/com/example/jdbcpostgresql/DBConfig.java
@@ -3,7 +3,6 @@ package com.example.jdbcpostgresql;
 import com.zaxxer.hikari.HikariConfig;
 import com.zaxxer.hikari.HikariDataSource;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
